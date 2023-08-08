60af0c1ab119659ed7747b1c3d111e37f4b5ce84
Elijah-05 <125945721+Elijah-05@users.noreply.github.com>
2023-08-08 11:07:41 +0300

    Realm Local Database Update

--- src/reduxToolkit/store.js
+++ src/reduxToolkit/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './features/counter/counterSlice'
 import productListReducer from './features/product/productListSlice'
 import draftItemReducer from './features/draftItems/draftItemSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         product: productListReducer,
         draftItem: draftItemReducer
     }
